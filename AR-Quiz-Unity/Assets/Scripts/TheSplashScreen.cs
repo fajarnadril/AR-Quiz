34ef3c7fd45eeab891b3d433ab06b52edaee0f4d
fajarbinus <36891062+fajarnadril@users.noreply.github.com>
2023-08-14 14:29:29 +0700

    Update 2023.08.02
    Solve this
    
    [DONE] 1.d waktu coba tes yg gain creators, dijawab salah tp munculnya tetap icon benar
    [DONE] 2. utk icon 3D yg stlh discan, lebih baik diubah utk menyesuaikan tema baru, jd jgn planet. aset yg dibutuhkan diinfo saja, nanti saya minta mhs magang buatkan. sepertinya lebih cocok gambar yg icon2 kecil spirit itu, bentuknya mungkin akan jd kaya badge yg agak2 semi 3D?
    [DONE] 3. tulisan GEMOI waktu game uda mulai ditaruh di antara time sama score saja.. skrg bentuknya kecil dan agak ke bawah.
    [DONE] button tanda tanya di kiri bawah belum berfungsi
    [DONE] di bagian akhir yg highscore thank you dll, itu backgroundnya sepertinya belum full, ada kelihatan gambar putih
    [DONE] titip tambahan logo GEE dan BEC di aplikasi

--- AR-Quiz-Unity/Assets/Scripts/TheSplashScreen.cs
+++ AR-Quiz-Unity/Assets/Scripts/TheSplashScreen.cs
@@ -1,41 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TheSplashScreen : MonoBehaviour
 {
     public GameObject[] imgs;
-    public float[] durationGanti;
-    // Start is called before the first frame update
+    public GameObject bg;
+    public AudioSource musicBg;
+    private float delay = 0f;
+    public float valuableDelay;
+    
     void Start()
     {
-        imgs[0].SetActive(true);
-        imgs[1].SetActive(true);
-        imgs[2].SetActive(false);
-        Invoke("MethodPertama", durationGanti[0]);
-    }
-
-    void MethodPertama()
-    {
-        imgs[0].SetActive(true);
-        imgs[1].SetActive(false);
-        imgs[2].SetActive(true);
-        Invoke("MethodKedua", durationGanti[1]);
-    }
-
-    void MethodKedua()
-    {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < imgs.Length; i++)
         {
-            Destroy(imgs[i]);
+            TurnOff();
+            StartCoroutine(TurnOn(i, delay));
+            delay += valuableDelay;       
         }
-
-
+        StartCoroutine(SplashEnd(Convert.ToInt32(valuableDelay) * imgs.Length));
+    }
+    void TurnOff()
+    {
+        for (int j = 0; j < imgs.Length; j++)
+        {
+            imgs[j].SetActive(false); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SplashEnd(int duration)
     {
-        
+      yield return new WaitForSeconds(duration);
+      bg.SetActive(false);
+      musicBg.Play();
+      TurnOff();
+    }
+
+    IEnumerator TurnOn(int imgID, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (imgID >= 0 && imgID < imgs.Length)
+        {
+           imgs[imgID].SetActive(true);
+        }
     }
 }
